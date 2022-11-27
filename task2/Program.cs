31de1d488032df1e24a50ade1372c3e8473a300e
Kate <Kate-pretty@ya.ru>
2022-11-27 22:23:41 +0500

    added

---
+++ task2/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите первое число:");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число:");
+int m = Convert.ToInt32(Console.ReadLine());
+int max = n;
+if (n > m) max = n;
+if (m > n) max = m;
+Console.Write("max=");
+Console.Write(max);
